030e5232dc22d6d78380a7a3587a314b4b8a66b6
BEATRIZ STHEFANIE SOUZA <beatriz.2810@aluno.pr.senac.br>
2023-11-22 15:31:08 -0300

    atualização

--- Atividadesphp/index.php
+++ Atividadesphp/index.php
@@ -1,22 +1,69 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="meu_estilo.css">
 </head>
+
 <body>
     <?php
     include("menu.php");
     ?>
+    </div>
+    <div class="container">
+        <div id="imagem1"><img class="img-fluid"src="img/feriasbr.jpg" alt="">
+        </div>
+        <br>
     
+        <div class="row">
+            <div class="col-6">
+                <h2>Passagens aéreas para todo Brasil</h2>
+            </div>
+            <div class="col-2">
+                <div class="card" style="width: 13rem;">
+                    <img src="img/bahia.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Elevador Lacerda</h5>
+                        <p class="card-text"> Ícone da cidade de Salvador, Bahia, no Brasil. Ele é um dos principais pontos turísticos da 
+                            cidade.</p>
+                        <a href="#" class="btn btn-primary">Ver pacotes</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-2">
+                <div class="card" style="width: 13rem;">
+                    <img src="img/riodejaneiro.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Rio de Janeiro</h5>
+                        <p class="card-text">Copacabana é um bairro famoso localizado na zona sul da cidade do Rio de Janeiro, Brasil. Conhecido por sua deslumbrante praia em forma de meia lua </p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-2">
+                <div class="card" style="width: 13rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
 
+        </div>
 
     </div>
-        <div id="imagem1"><img src="img/feriasbr.jpg" alt=""></div>
 
 </body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+    crossorigin="anonymous"></script>
+
 </html>
